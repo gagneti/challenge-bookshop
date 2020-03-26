3d8009c4a1decc72f04f52222a51ba60a58d198c
Giuseppe Agneti <giuseppe.agneti@accenture.com>
2020-03-26 18:08:44 +0100

    installed prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
